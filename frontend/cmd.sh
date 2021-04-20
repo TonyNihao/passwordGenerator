c6244dbea66ec056d18f0719f7bf00895ede23f5
tony <aprylypa@mail.com>
2021-04-20 12:05:09 +0000

    fix cmd.sh script

--- frontend/cmd.sh
+++ frontend/cmd.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
 
-if [ "ENV" = "DEV"];then
+
+if [ "ENV" = "DEV" ];then
   exec python /app/app.py
 else
-  exec uwsgi --http 7777 --wsgi-file /app/app.py --callable app
+  exec uwsgi --http 9090 --wsgi-file /app/app.py --callable app
 fi
