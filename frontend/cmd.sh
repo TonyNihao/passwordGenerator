572b9eae4ba25fb3ab5f4ceb6c659fd51729b18b
tony <aprylypa@mail.com>
2021-04-20 12:09:13 +0000

    fix uwsgi parameters

--- frontend/cmd.sh
+++ frontend/cmd.sh
@@ -5,5 +5,5 @@ set -e
 if [ "ENV" = "DEV" ];then
   exec python /app/app.py
 else
-  exec uwsgi --http 9090 --wsgi-file /app/app.py --callable app
+  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/app.py --callable app
 fi
