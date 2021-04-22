9241fb94c20ff28c8fa2c24d5c49db88a2f8e143
tony <aprylypa@gmail.com>
2021-04-22 07:46:24 +0000

    prepare to ci pipline

--- backend/cmd.sh
+++ backend/cmd.sh
@@ -4,6 +4,8 @@ set -e
 
 if [ "$ENV" = "DEV" ];then
   exec python /app/app.py
+elif [ "$ENV" = "UNIT" ];then
+  exec python /app/tests.py
 else
   exec uwsgi --http 0.0.0.0:7777 --wsgi-file /app/app.py --callable app
 fi
