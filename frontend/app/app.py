7bd680cbc76cfb7fedfea41d3e0c0c8e4560347d
tony <aprylypa@mail.com>
2021-04-23 10:55:01 +0300

    fix connection error

--- frontend/app/app.py
+++ frontend/app/app.py
@@ -7,24 +7,20 @@ app = Flask(__name__)
 @app.route('/', methods=['POST','GET'])
 def index():
     string = None
+    backend = 'pwdgen_backend:7777'
+
     if request.method == 'POST':
         uppercase = True if request.form.get('uppercase') else False
         digits = True if request.form.get('digits') else False
         symbols = True if request.form.get('symbols') else False
         length = request.form.get('length')
-
-        string = requests.get('http://localhost:5000/generate/&length={0}&digits={1}&symbols={2}&uppercase={3}'.format(
+        string = requests.get('http://{0}/&length={1}&digits={2}&symbols={3}&uppercase={4}'.format(
+                                                                            backend,
                                                                             length,
                                                                             digits,
                                                                             symbols,
                                                                             uppercase)).text
     return render_template('index.html', string=string)
-
-
-@app.route('/generate/<query>')
-def generate(query):
-    backend = 'pwdgen_backend:7777'
-    return requests.get('http://{0}/{1}'.format(backend,query)).text
 
 
 if __name__ == '__main__':
